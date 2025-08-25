43ec16681db2c607723f2da21ba0f4e459469377
changDDAO <younch8342@naver.com>
2025-08-25 13:00:30 +0900

    INIT

---
+++ src/test/java/com/changddao/plannerservice/PlannerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changddao.plannerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
